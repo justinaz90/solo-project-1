bf068034b65f36ca4ad3abb4fd3328aa1c450334
Justyna Szewczyk <justyna0829@gmail.com>
2021-03-03 19:50:04 +0100

    Add chart js

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+
 {
   /* Sidebar */
 
@@ -9,4 +12,58 @@
     e.preventDefault();
     toggleMenu();
   });
+
+
+  /* Chart */
+  function initChart(){
+    const ctx = document.getElementById('myChart').getContext('2d');
+
+    const chart = new Chart(ctx, {
+      // 1
+      type: 'bar',
+      data: {
+        // 2
+        labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'],
+        // 3
+        datasets: [{
+          // 4
+          label: 'Signups',
+          // 5
+          backgroundColor: '#8DBEC8',
+          borderColor: '#8DBEC8',
+          // 6
+          data: [ 350, 200, 220, 340, 420, 400, 300, 280, 310 ],
+        },
+        {
+          label: 'FTD',
+          backgroundColor: '#F29E4E',
+          borderColor: '#F29E4E',
+          data: [ 400, 150, 300, 270, 450, 110, 200, 490, 320 ],
+        },
+        {
+          label: 'Earned',
+          backgroundColor: '#71B374',
+          borderColor: '#71B374',
+          data: [ 300, 200, 170, 300, 340, 220, 90, 150, 170 ],
+          // 7
+          hidden: true,
+        }]
+      },
+      options: {
+        responsive: true,
+        legend: {
+          color: '#979797',
+          labels: {
+            usePointStyle: true,
+            fontFamily: 'Roboto, sans-serif',
+            fontColor: '#80BFCA',
+            fontSize: 16,
+            padding: 40,
+          }
+        }
+      },
+    });
+  }
+
+  initChart();
 }
