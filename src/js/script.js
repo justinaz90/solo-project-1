eb9205b8e0958d5735c7cc80815e1604012bfe1d
Justyna Szewczyk <justyna0829@gmail.com>
2021-03-05 20:34:07 +0100

    Start with modals

--- src/js/script.js
+++ src/js/script.js
@@ -114,4 +114,41 @@
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
+
+
+  /* Modals */
+
+  function closeModal() {
+    document.getElementById('overlay').classList.remove('show');
+  }
+
+  document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+      e.preventDefault();
+      closeModal();
+    });
+  });
+
+  document.querySelector('#overlay').addEventListener('click', function(e) {
+    if(e.target === this) {
+      closeModal();
+    }
+  });
+
+  document.addEventListener('keyup', function(e) {
+    if(e.keyCode === 27) {
+      closeModal();
+    }
+  });
+
+
+  function openModal(modal) {
+    document.querySelectorAll('#overlay > *').forEach(function(modal) {
+      modal.classList.remove('show');
+    });
+    document.querySelector('#overlay').classList.add('show');
+    document.querySelector(modal).classList.add('show');
+  }
+
+  //openModal('#quit');
 }
